492f445eb4da116256d9d4443ba6a9110c696e6a
wkajjumba <wkajjumba@kamwenge.com>
2018-10-16 00:45:40 -0500

    gems for H

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
+//= require jquery
 //= require rails-ujs
-//= require bootstrap-sprockets
+//= require turbolinks
+//= require foundation
 //= require_tree .
+$(function() {
+  $(document).foundation();
+});
